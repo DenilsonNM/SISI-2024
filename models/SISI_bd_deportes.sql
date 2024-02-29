dd62a108fc6fab7ad5e2f661b18de140b6d976a1
uriel211100 <148598944+uriel211100@users.noreply.github.com>
2024-02-29 15:01:30 -0600

    Update SISI_bd_deportes.sql

--- models/SISI_bd_deportes.sql
+++ models/SISI_bd_deportes.sql
@@ -1,43 +1,96 @@
--- CREATE DATABASE bd_deportes2; 
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-02-2024 a las 00:34:40
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
 
--- use bd_deportes2; 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_deportes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `det_deportes`
+--
+
+CREATE TABLE `det_deportes` (
+  `id_deporte` int(3) NOT NULL,
+  `id` int(3) NOT NULL,
+`nombre_representante` varchar(100) NOT NULL,
+  `dia` varchar(100) NOT NULL,
+  `horario` varchar(100) NOT NULL,
+  `lugar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `deportes`
+--
 
 CREATE TABLE `deportes` (
   `id` int(3) NOT NULL,
-  `Nombre` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nombre_deporte` varchar(30) COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+--
+-- Índices para tablas volcadas
+--
 
-CREATE TABLE `detalle_deportes` (
-  `id_deporte` int(3) NOT NULL,
-	`consecutivo` int(3) NOT NULL,
-  `dia` varchar(15) NOT NULL,
-  `horario` varchar(15) NOT NULL,
-  `lugar` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Indices de la tabla `det_deportes`
+--
+ALTER TABLE `det_deportes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_deporte` (`id_deporte`);
 
+--
+-- Indices de la tabla `deportes`
+--
 ALTER TABLE `deportes`
   ADD PRIMARY KEY (`id`);
-  
-  
-  ALTER TABLE `detalle_deportes`
-  ADD PRIMARY KEY (`consecutivo`),
-  ADD KEY `id_deporte` (`id_deporte`);
-  
-  
-  ALTER TABLE `deportes`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
-  
-  
-  ALTER TABLE `detalle_deportes`
-  MODIFY `consecutivo` int(3) NOT NULL AUTO_INCREMENT;
-  
-  
-  ALTER TABLE `detalle_deportes`
-  ADD CONSTRAINT `detalle_deportes_ibfk_1` FOREIGN KEY (`id_deporte`) REFERENCES `deportes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-	COMMIT;
-    
---  select *from deportes; 
 
--- select*from detalle_deportes;   
-    
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `det_deportes`
+--
+ALTER TABLE `det_deportes`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `deportes`
+--
+ALTER TABLE `deportes`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `det_deportes`
+--
+ALTER TABLE `det_deportes`
+  ADD CONSTRAINT `det_deportes_ibfk_1` FOREIGN KEY (`id_deporte`) REFERENCES `deportes` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
